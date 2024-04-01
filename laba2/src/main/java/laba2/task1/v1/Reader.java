24dd858b3f8f6bdc223dedf5431110a7d633a28e
Maksym Horelko <maksym.horelko@gmail.com>
2024-04-01 14:49:12 +0300

    task1_v1 complete in lw2

---
+++ laba2/src/main/java/laba2/task1/v1/Reader.java
@@ -0,0 +1,16 @@
+package laba2.task1.v1;
+
+import java.io.Serializable;
+
+public class Reader extends Person implements Serializable {
+	private static final long serialVersionUID = -4507195945999952518L;
+
+	public Reader(String firstname, String lastname, Long readerID) {
+		super(firstname, lastname, readerID);
+	}
+
+	@Override
+	public String toString() {
+		return "Reader{" + "id=" + getId() + ", firstname=" + getFirstname() + ", lastname=" + getLastname() + "}";
+	}
+}
