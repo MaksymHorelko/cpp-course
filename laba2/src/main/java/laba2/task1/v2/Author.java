5f504b456e8693de1fdb496b8e523f48ed7a2662
Maksym Horelko <maksym.horelko@gmail.com>
2024-04-08 15:00:55 +0300

    task1_v2 complete in lw2

---
+++ laba2/src/main/java/laba2/task1/v2/Author.java
@@ -0,0 +1,20 @@
+package laba2.task1.v2;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Author extends Person {
+
+	public Author(String firstname, String lastname, Long authorId) {
+		super(firstname, lastname, authorId);
+	}
+
+	public static Author readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+		return new Author((String) in.readObject(), (String) in.readObject(), (Long) in.readObject());
+	}
+
+	@Override
+	public String toString() {
+		return "Author{" + "id=" + getId() + ", firstname=" + getFirstname() + ", lastname=" + getLastname() + "}";
+	}
+}
